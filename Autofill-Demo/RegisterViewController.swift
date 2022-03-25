5f460279fd47c8eb3b7e2191cbf9264f89477871
Alan Liu <ryutenu.tokyo@gmail.com>
2022-03-26 01:43:08 +0900

    First commit

---
+++ Autofill-Demo/RegisterViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RegisterViewController.swift
+//  Autofill-Demo
+//
+//  Created by Alan Liu on 2022/03/23.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var email: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        email.textContentType = .emailAddress
+        email.keyboardType = .emailAddress
+        
+        password.textContentType = .password
+    }
+}
